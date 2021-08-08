b46da270c2f18c7ccbceaedfdbfbd089539ead16
lushengli-1 <2820724424@qq.com>
2021-08-08 22:00:19 +0800

    第二次

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, {Component} from "react";
-import {Button,message} from 'antd'
-//import 'antd/dist/antd.css'
+import {BrowserRouter, Route,Switch} from 'react-router-dom'
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
 
 export default class App extends Component{
-  handleClick = () => {
-    message.success('成功。。。。。。。。。。。')
-  }
+ 
 render(){
   return (
-    <div>
-    <div>gdgg</div>
-    <Button type='primary' onClick={this.handleClick}>dff</Button>
-  </div>
+    <BrowserRouter>
+    <Switch>{/* 只匹配其中一个 */}
+      <Route path='/login' component={Login}></Route>
+      <Route path='/' component={Admin}></Route>
+    </Switch>
+    </BrowserRouter>
   )
 }
 }
